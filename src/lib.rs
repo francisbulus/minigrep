2c85d528e4f73195446d820acc16875fadbbe980
“Francis <notchera@gmail.comssh-keygen -t rsa -C notchera@gmail.com>
2022-02-18 10:52:06 +0100

    add test for search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,35 @@ use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
 
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub struct Config {
     query: String,
     filename: String
